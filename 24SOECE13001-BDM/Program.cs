194a232410a844049ba27eae3b220882187fb93a
bansu_aghera <agherabansi2@gmail.com>
2025-07-04 11:29:49 +0530

    Add project files.

---
+++ 24SOECE13001-BDM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_MAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the NO 1 : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the NO 2 :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            sum = a + b;
+            Console.WriteLine($"sum is {a} + {b} : " + sum);
+
+            Console.Read();
+        }
+    }
+}
---
+++ 24SOECE13001-BDM/hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13001_BDM
+{
+    internal class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
